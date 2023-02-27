57b6147003f207c32f0b4a1cced9b51896002972
juanelco77 <jalea.naranjax@gmail.com>
2023-02-27 09:04:15 -0300

    first commit

---
+++ comandos/utilidades/ping.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js")
+
+module.exports = {
+  name: "ping",
+  description: "Mira la latencia del bot",
+  type: Discord.ApplicationCommandType.ChatInput,
+
+  run: async (client, interaction) => {
+
+    let ping = client.ws.ping;
+
+    let embed_1 = new Discord.EmbedBuilder()
+    .setAuthor({ name: client.user.username, iconURL: client.user.displayAvatarURL({ dynamic: true }) })
+    .setDescription(`Pong! ${interaction.user}, El ping del bot esta en \`calculando...\`.`)
+    .setColor("Random");
+
+    let embed_2 = new Discord.EmbedBuilder()
+    .setAuthor({ name: client.user.username, iconURL: client.user.displayAvatarURL({ dynamic: true }) })
+    .setDescription(`Pong! ${interaction.user}, El ping del bot esta en \`${ping}ms\`.`)
+    .setColor("Random");
+
+    interaction.reply({ embeds: [embed_1] }).then( () => {
+        setTimeout( () => {
+            interaction.editReply({ embeds: [embed_2] })
+        }, 2000)
+    })
+  }
+}
